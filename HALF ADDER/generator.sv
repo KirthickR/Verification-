f5ad537555a133533f0ac2af36a2855ac4b9748c
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-04-04 09:47:57 +0530

    Create generator.sv

---
+++ HALF ADDER/generator.sv
@@ -0,0 +1,22 @@
+class generator;
+  transaction trans;
+  mailbox gen2drv;
+  
+  function new(mailbox gen2drv);
+    this.gen2drv=gen2drv;
+  endfunction
+  
+  task main();
+      trans=new();
+    
+    for(int i=0;i<4;i++)begin
+        trans.randomize();
+        
+        trans.display("GENERATOR SIGNALS");
+        gen2drv.put(trans);
+      #5;
+      end
+    
+  endtask
+  
+endclass
