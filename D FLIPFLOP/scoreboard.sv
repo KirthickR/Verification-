6f8fe73803d4fb7f7b0853ca335ddc3c4a2240a1
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-04-08 16:02:43 +0530

    Create scoreboard.sv

---
+++ D FLIPFLOP/scoreboard.sv
@@ -0,0 +1,27 @@
+class scoreboard;
+  transaction trans;
+  mailbox mon2scb;
+  event my_event;
+  
+  function new(mailbox mon2scb );
+    this.mon2scb=mon2scb;
+  endfunction 
+  
+  task main();
+    trans=new();
+    repeat(6)begin
+     
+
+      mon2scb.get(trans);
+      trans.display("SCOREBOARD CLASS SIGNALS");
+      
+      if(((trans.d == trans.q) && (trans.qbar == ~trans.q)) ||  ((!trans.rst) && (trans.q == 0) && (trans.qbar == 1)))
+        $display("STATUS:Testcase Pass");
+      else
+        $display("STATUS:Testcase fail");
+      ->my_event;
+      trans.display_l();
+      
+    end
+  endtask
+endclass
