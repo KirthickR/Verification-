9d28ffde139f84f57f38492c04f524beee0d9d42
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-04-15 15:13:19 +0530

    Create generator.sv

---
+++ UP_DOWN_COUNTER/generator.sv
@@ -0,0 +1,19 @@
+class generator;
+  transaction trans;
+  mailbox gen2drv;
+  event my_event;
+  
+  function new(mailbox gen2drv);
+    this.gen2drv=gen2drv;
+  endfunction
+  
+  task main;
+    trans=new();
+    repeat(20)begin
+      trans.randomize();
+      trans.display("GENERATOR CLASS SIGNALS");
+      gen2drv.put(trans);
+      @my_event;
+    end
+  endtask
+endclass
